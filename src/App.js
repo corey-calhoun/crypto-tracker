0022d0c82794d31feb23eccb183a3a75531eea7b
Corey Calhoun <corey.calhoun@dkgconceptsllc.com>
2020-10-22 00:19:46 -0500

    --- Auto-Git Commit ---

--- src/App.js
+++ src/App.js
@@ -5,6 +5,17 @@ import './App.css';
 // components 
 
 function App() {
+
+  const [country, setInputCountry] = useState("worldwide");
+  const [stockInfo, setStockInfo] = useState({});
+  const [stocks, setStocks] = useState([]);
+  const [tableData, setTableData] = useState([]);
+  const [casesType, setCasesType] = useState("cases");
+  const [mapCenter, setMapCenter] = useState({ lat: 34.80746, lng: -40.4796 });
+  const [mapZoom, setMapZoom] = useState(3);
+
+
+
   return (
     <div className="app">
       <div className="app__left">
